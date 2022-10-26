e990e871dce6a271e8f46a1bc0406012e0c6c692
Arima <mikokunsei@gmail.com>
2022-10-26 16:11:31 +0700

    delete unused

--- admin/layouts/header.php
+++ admin/layouts/header.php
@@ -40,7 +40,7 @@ if (!isset($_SESSION['username'])) {
     <!-- Left navbar links -->
     <ul class="navbar-nav">
       <li class="nav-item">
-        <a class="nav-link" data-widget="pushmenu" href="template/#" role="button"><i class="fas fa-bars"></i></a>
+        <a class="nav-link" data-widget="pushmenu" href="#" role="button"><i class="fas fa-bars"></i></a>
       </li>
       <li class="nav-item d-none d-sm-inline-block">
         <a href="dashboard" class="nav-link">Home</a>
